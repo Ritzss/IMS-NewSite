634fb5be8ab1a3482fca5b32518d3aa87ca8b8f2
Ritzss <ritanshubabuta399@gmail.com>
2026-01-20 18:45:34 +0530

    version 1.1

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,6 @@ import './globals.css'
 export const metadata = {
   title: 'Vastra IMS',
   description: 'A simple Inventory Management System',
-   icons: {
-    icon: "\favicon.ico",
-  },
 }
 
 export default function RootLayout({ children }) {
